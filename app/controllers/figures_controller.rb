a4342905edf26146121b7e28794cae0621620543
BebopVinh <vinhdo07@gmail.com>
2019-04-07 01:51:26 -0700

    fixing /figures/new

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,43 @@
 class FiguresController < ApplicationController
-  # add controller methods
+   get '/figures' do
+      erb :'/figures/index'
+   end
+  
+   get '/figures/new' do
+      @landmarks = Landmark.all
+      @titles = Title.all
+      erb :'/figures/new'
+   end
+
+   post '/figures' do
+      @figure = Figure.create(params[:figure])
+      unless params[:title][:name].empty?
+         @figure.titles << Title.new(params[:title])
+      end
+
+      unless params[:landmark][:name].empty?
+         @figure.landmarks << Landmark.new(params[:landmark])
+      end
+      @figure.save
+      redirect to("/figures/#{@figure.id}")
+   end
+
+   get '/figures/:id' do
+      @figure = Figure.find(params[:id])
+      erb :'/figures/show'
+   end
+
+   post '/figures/:id/edit' do
+      redirect "/figures/#{params[:id]}/edit"
+   end
+
+   get '/figures/:id/edit' do
+      @figure = Figure.find(params[:id])
+      erb :'/figures/edit'
+   end
+
+   post '/figures/:id' do
+      binding.pry
+      redirect '/figures/:id'
+   end
 end
